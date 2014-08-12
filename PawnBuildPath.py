6c2a84473b04c53f31e0edde390f2de87ca59591
Southclaw <SouthclawJK@gmail.com>
2014-08-12 09:34:47 +0100

    Added user friendly build settings
    Preferences > Package Settings > Pawn Compiler Settings > Build Settings
    
    Prompts for user to enter Pawn compiler path then automatically fills
    and prompts for save a .sublime-build file.
    
    Removed the theme since it's not relevant.

---
+++ PawnBuildPath.py
@@ -0,0 +1,21 @@
+import sublime, sublime_plugin
+from .edit import Edit
+
+class PawnBuildPathCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.window().show_input_panel("Set Pawn build path", "C:\\Pawno\\", self.onPawnPathDone, None, None)
+
+
+	def onPawnPathDone(self, input):
+		view = self.view.window().new_file()
+
+		with Edit(view) as edit:
+			edit.insert(0,
+'{\n\
+	"cmd": ["pawncc.exe", "$file", "-o$file_path\\\\$file_base_name", "-;+", "-(+", "-d3"],\n\
+	"file_regex": "(.*?)[(]([0-9]*)[)]",\n\
+	"working_dir": "'+input+'"\n\
+}')
+
+		view.set_name("Pawn.sublime-build")
+		view.run_command("save");
