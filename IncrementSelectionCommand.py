7c8cdc802d5328a194e575b4ad13459d3cf007be
Southclaw <southclawjk@gmail.com>
2014-11-19 13:42:14 +0000

    Added number sequence command
    Inserts an incremental sequence of numbers at cursor positions
    throughout the file. Bound to CTRL+Shift+R by default.

---
+++ IncrementSelectionCommand.py
@@ -0,0 +1,12 @@
+import sublime, sublime_plugin
+
+class SequentialNumberInsertCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+
+		value = 0
+
+		for selection in self.view.sel():
+			self.view.erase(edit, selection)
+			self.view.insert(edit, selection.begin(), str(value))
+			value = value + 1
