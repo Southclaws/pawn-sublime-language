1916b03ba0f61f488637310c7608b3244fc80c0e
Southclaw <southclawjk@gmail.com>
2016-07-03 10:47:45 +0100

    Updated build settings generator
    Improved file_regex to support multi-line errors.

--- PawnBuildPath.py
+++ PawnBuildPath.py
@@ -6,17 +6,17 @@ class PawnBuildPathCommand(sublime_plugin.TextCommand):
 		self.view.window().show_input_panel("Set Pawn build path", "C:\\Pawno\\", self.onPawnPathDone, None, None)
 
 
-	def onPawnPathDone(self, input):
+	def onPawnPathDone(self, path):
 		view = self.view.window().new_file()
+		path = path.replace("\\", "/")
 
 		with Edit(view) as edit:
-			edit.insert(0,
-'{\n\
-	"cmd": ["pawncc.exe", "$file", "-o$file_path\\\\\\\\$file_base_name", "-;+", "-(+", "-d3"],\n\
-	"file_regex": "(.*?)[(]([0-9]*)[)]",\n\
-	"selector": "source.pwn",\n\
-	"working_dir": "'+input.replace("\\", "\\\\")+'"\n\
-}')
+			edit.insert(0,"""{
+	"cmd": ["pawncc.exe", "$file", "-o$file_path/$file_base_name", "-;+", "-(+", "-d3"],
+	"file_regex": "(.*?)\\\\(([0-9]*)[- 0-9]*\\\\)",
+	"selector": "source.pwn",
+	"working_dir": "%s"
+}"""%(path))
 
 		view.set_name("Pawn.sublime-build")
 		view.run_command("save");
